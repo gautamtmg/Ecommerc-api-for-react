4c4822a09627ac464660d043b488fc76b60802bf
unknown <gautam.tamang0000@gmail.com>
2021-05-18 15:56:24 +0545

    Backend User authentication JSON Web tokens

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path 
 from . import views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register', views.registerUser, name='register'),
+  
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>/', views.getProduct, name= "product"),
+
+
+    path('users/profile/', views.getUserProfile, name="user-profile"),
+    path('users/', views.getUsers, name="users"),
+
 ]
